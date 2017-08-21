20caeff5a47300aa04cc6964f8403cc2c02391ee
Mario Badr <mario.badr@outlook.com>
2017-08-21 12:00:02 -0400

    Add example for using pam algorithm

---
+++ examples/pam-1D/main.cpp
@@ -0,0 +1,34 @@
+#include <clupp/distance.hpp>
+#include <clupp/pam.hpp>
+
+#include <iostream>
+
+int main(int argc, char ** argv)
+{
+  if(argc != 2) {
+    std::cout << "Missing Argument: please enter the number of clusters to create.\n";
+    return EXIT_SUCCESS;
+  }
+
+  auto const k = static_cast<int>(std::strtol(argv[1], nullptr, 10));
+
+  // build a 1-dimensional matrix of objects
+  Eigen::VectorXd data(8);
+  data << 942, 2633, 2654, 2137, 373, 434, 1495, 1230;
+
+  // group the data into k clusters
+  auto const result = clupp::partition_around_medoids(k, data);
+
+  std::cout << "Medoids:\n";
+  for(auto const &medoid: result.medoids) {
+    std::cout << medoid << "\n";
+  }
+
+  std::cout << "\nGrouping:\n";
+  for(int i = 0; i < data.rows(); ++i) {
+    std::cout << data(i) << " assigned to medoid " << result.classification[i] << "\n";
+  }
+
+
+  return EXIT_SUCCESS;
+}
---
+++ options.cmake
@@ -0,0 +1,7 @@
+# potential build options
+
+option(
+  CLUPP_BUILD_EXAMPLES
+  "Build the example executables that use clupp"
+  OFF
+)
